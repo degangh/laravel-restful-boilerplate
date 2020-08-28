87dbe9901e4db0a014c68603608140e9a675e65b
unknown <nicolash@fusion-solutions.com.au>
2020-08-28 14:01:31 +0930

    read application name from env file as app title

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{csrf_token()}}">
-        <title>换热器设备</title>
+        <title>{{config('app.name')}}</title>
         <link href=" {{ mix('css/app.css') }}" rel="stylesheet">
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
